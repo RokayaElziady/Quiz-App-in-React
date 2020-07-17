14e231abc401455591e00e3764168d5799fc82a5
Rokaya Elziady <rokayaelziady@gmail.com>
2020-07-17 15:12:22 +0200

    quiz service

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDom from "react-dom";
+import QuizBee from './components/QuizBee';
+import "./assets/style.css";
+
+ReactDom.render(<QuizBee/>,document.getElementById("root"));
